eb16717f6dac37c28bad771a1c5b070598123256
xiaowei <xiaowei20150509YIUHLK>
2021-07-13 14:23:19 +0800

    OFDEditor demo

---
+++ OFDEditor/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+	//设置全局字体样式
+	QFont font;
+	font.setPointSize(11);
+	font.setFamily(QStringLiteral("宋体"));
+	a.setFont(font);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
